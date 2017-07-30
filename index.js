fab4eeaee201eb6bcc81a3bb5bc4be0f98c417cc
Simon Blackwell <syblackwell@anywhichway.com>
2017-07-30 11:04:18 -0400

    Modified internal handler to immediately set a timeout and return. Maximizes load that can be handled at the expense of RAM.

--- index.js
+++ index.js
@@ -21,91 +21,93 @@
 	class StoreServer {
 		constructor(storage,options={accept:["clear","count","compress","key"],methods:["POST","GET", "PUT", "DELETE", "OPTIONS"]}) {
 			const handler = (request,response) => {
-				const urlpath = request.url.substring(0,(request.url.indexOf("?")>0 ? request.url.indexOf("?") : request.url.length));
-			    if(urlpath==="/remotestore.js") {
-			    	 fs.readFile(path.normalize([__dirname,"node_modules","remotestore","index.js"].join("/")), function(error, content) {
-			 	        if (error) {
-			 	        	let message, code;
-			 	            if(error.code == "ENOENT"){
-			 	            	code = 404;
-			 	                message = urlpath + " not found"+" ..\n";
-			 	            }
-			 	            else {
-			 	                code = 500;
-			 	                message = "Sorry, check with the site admin for error: "+error.code+" ..\n";
-			 	            }
-			 	            console.log(code,error.code,path.normalize([__dirname,urlpath].join("/")));
-			 	            response.writeHead(code);
-			 	            response.end(message);
-			 	            response.end();
-			 	        }
-			 	        else {
-			 	            response.writeHead(200, { "Content-Type": "text/javascript" });
-			 	            response.end(content, "utf-8");
-			 	        }
-			 	    });
-			    	 return;
-			    }
-			    const id = urlpath.substring(1);
-		    	if(!options.methods.includes(request.method)) {
-		    		const headers = {};
-	      	      	headers["Access-Control-Allow-Origin"] = "*";
-		    		response.writeHead(405,headers);
-		    	    response.end("Method Not Allowed");
-		    	    return;
-		    	}
-		    	if (request.method === "OPTIONS") {
-		    	      const headers = {};
-		    	      headers["Access-Control-Allow-Origin"] = "*";
-		    	      headers["Access-Control-Allow-Methods"] = options.methods.join(",");
-		    	      headers["Access-Control-Allow-Credentials"] = false;
-		    	      headers["Access-Control-Max-Age"] = "86400"; // 24 hours
-		    	      headers["Access-Control-Allow-Headers"] = "Content-Type, Content-Length, Authorization, Accept, User-Agent, X-Requested-With";
-		    	      response.writeHead(200, headers);
-		    	      response.end();
-		    	} else if(request.method==="GET") {
-			    	storage.get(id).then((result) => {
+				setTimeout(() => {
+					const urlpath = request.url.substring(0,(request.url.indexOf("?")>0 ? request.url.indexOf("?") : request.url.length));
+				    if(urlpath==="/remotestore.js") {
+				    	 fs.readFile(path.normalize([__dirname,"node_modules","remotestore","index.js"].join("/")), function(error, content) {
+				 	        if (error) {
+				 	        	let message, code;
+				 	            if(error.code == "ENOENT"){
+				 	            	code = 404;
+				 	                message = urlpath + " not found"+" ..\n";
+				 	            }
+				 	            else {
+				 	                code = 500;
+				 	                message = "Sorry, check with the site admin for error: "+error.code+" ..\n";
+				 	            }
+				 	            console.log(code,error.code,path.normalize([__dirname,urlpath].join("/")));
+				 	            response.writeHead(code);
+				 	            response.end(message);
+				 	            response.end();
+				 	        }
+				 	        else {
+				 	            response.writeHead(200, { "Content-Type": "text/javascript" });
+				 	            response.end(content, "utf-8");
+				 	        }
+				 	    });
+				    	 return;
+				    }
+				    const id = urlpath.substring(1);
+			    	if(!options.methods.includes(request.method)) {
 			    		const headers = {};
 		      	      	headers["Access-Control-Allow-Origin"] = "*";
-		      	      	response.writeHead(200, headers);
-			    		response.end(JSON.stringify(result));
-			    	})
-			    } else if(request.method==="PUT") {
-			    	getBody(request).then((body) => {
-			    		storage.set(id,body).then(() => {
-			    			const headers = {};
-			      	      	headers["Access-Control-Allow-Origin"] = "*";
-			      	      	response.writeHead(200, headers);
-			      	      	response.end();
-				    	})
-			    	});
-			    } else if(request.method==="DELETE") {
-			    	storage.delete(id).then((result) => {
-			    		response.end(result);
-			    	})
-			    } else if(request.method==="POST") {
-			    	if(!options.accept.includes(id)) {
-			    		const headers = {};
-		      	      	headers["Access-Control-Allow-Origin"] = "*";
-			    		response.writeHead(403,headers);
-			    	    response.end("Forbidden");
+			    		response.writeHead(405,headers);
+			    	    response.end("Method Not Allowed");
 			    	    return;
 			    	}
-			    	getBody(request).then((body) => {
-			    		const args = JSON.parse(body);
-			    		storage[id](...args).then((result) => {
-			    			const headers = {};
+			    	if (request.method === "OPTIONS") {
+			    	      const headers = {};
+			    	      headers["Access-Control-Allow-Origin"] = "*";
+			    	      headers["Access-Control-Allow-Methods"] = options.methods.join(",");
+			    	      headers["Access-Control-Allow-Credentials"] = false;
+			    	      headers["Access-Control-Max-Age"] = "86400"; // 24 hours
+			    	      headers["Access-Control-Allow-Headers"] = "Content-Type, Content-Length, Authorization, Accept, User-Agent, X-Requested-With";
+			    	      response.writeHead(200, headers);
+			    	      response.end();
+			    	} else if(request.method==="GET") {
+				    	storage.get(id).then((result) => {
+				    		const headers = {};
 			      	      	headers["Access-Control-Allow-Origin"] = "*";
 			      	      	response.writeHead(200, headers);
-			      	      	if(typeof(result)!=="undefined") {
-			      	      		response.end(JSON.stringify(result));
-			      	      	} else {
-			      	      		response.end();
-			      	      	}
+				    		response.end(JSON.stringify(result));
 				    	})
-			    	});
-			    }
-			}
+				    } else if(request.method==="PUT") {
+				    	getBody(request).then((body) => {
+				    		storage.set(id,body).then(() => {
+				    			const headers = {};
+				      	      	headers["Access-Control-Allow-Origin"] = "*";
+				      	      	response.writeHead(200, headers);
+				      	      	response.end();
+					    	})
+				    	});
+				    } else if(request.method==="DELETE") {
+				    	storage.delete(id).then((result) => {
+				    		response.end(result);
+				    	})
+				    } else if(request.method==="POST") {
+				    	if(!options.accept.includes(id)) {
+				    		const headers = {};
+			      	      	headers["Access-Control-Allow-Origin"] = "*";
+				    		response.writeHead(403,headers);
+				    	    response.end("Forbidden");
+				    	    return;
+				    	}
+				    	getBody(request).then((body) => {
+				    		const args = JSON.parse(body);
+				    		storage[id](...args).then((result) => {
+				    			const headers = {};
+				      	      	headers["Access-Control-Allow-Origin"] = "*";
+				      	      	response.writeHead(200, headers);
+				      	      	if(typeof(result)!=="undefined") {
+				      	      		response.end(JSON.stringify(result));
+				      	      	} else {
+				      	      		response.end();
+				      	      	}
+					    	})
+				    	});
+				    }
+				});
+			};
 			this.httpServer = require("http").createServer(handler);
 		}
 		listen(port,cb) {
